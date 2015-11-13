1c16876117fa683c77c9945728da511e2d856af9
akano-yuki <akano_yuki@cyberagent.co.jp>
2015-11-13 15:05:06 +0900

    userController修正

--- src/main/java/totec/spring_boot/simple/controller/UserController.java
+++ src/main/java/totec/spring_boot/simple/controller/UserController.java
@@ -1,5 +1,6 @@
 package totec.spring_boot.simple.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author akano_yuki on 2015/11/13.
  */
 @RequestMapping("/user")
-@RestController
+@Controller
 public class UserController {
 
 	@RequestMapping(value = "/{userId}")
@@ -19,6 +20,6 @@ public class UserController {
 		model.addAttribute("userPublicScore", 10);
 		model.addAttribute("userImage", "usfd2e1839d62dd2a9bb437b16588de0deab78057f.png");
 
-		return "/user";
+		return "/content/user";
 	}
 }
